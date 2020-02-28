afc15ce3d5430fbae644d383a5119a3fb2840ae5
Virulious <virulious@hotmail.com>
2020-02-28 09:24:43 -0500

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,16 +1,22 @@
 let cards = ["queen", "queen", "king", "king"];
 let cardsInPlay = [];
-let cardOne = cards[0];
-let cardTwo = cards[2];
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
 
-if (cardsInPlay.length === 2) {
+function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert('You found a match!');
-	}
+  		console.log("You found a match!");
+	} 
 	else {
-		alert('Sorry, Try again.');
+  		console.log("Sorry, try again.");
 	}
 }
 
+function flipCard(cardId) {
+	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId]);
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}
+}
+
+flipCard(0);
+flipCard(2);
