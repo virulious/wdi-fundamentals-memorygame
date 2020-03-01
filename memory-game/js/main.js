c851e93c7dcb9bfee7e5985854863f4a0afe1e91
Virulious <virulious@hotmail.com>
2020-03-01 06:09:59 -0500

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,4 +1,25 @@
-let cards = ["queen", "queen", "king", "king"];
+let cards = [
+{
+	rank: 'queen',
+	suit: 'hearts',
+	cardImage: 'images/queen-of-hearts.png'
+},
+{
+	rank: 'queen',
+	suit: 'diamonds',
+	cardImage: 'images/queen-of-diamonds.png'
+},
+{
+	rank: 'king',
+	suit: 'hearts',
+	cardImage: 'images/king-of-hearts.png'
+},
+{
+	rank: 'king',
+	suit: 'diamonds',
+	cardImage: 'images/king-of-diamonds.png'
+}	
+];
 let cardsInPlay = [];
 
 function checkForMatch() {
@@ -6,13 +27,15 @@ function checkForMatch() {
   		console.log("You found a match!");
 	} 
 	else {
-  		console.log("Sorry, try again.");
+  		alert("Sorry, try again.");
 	}
 }
 
 function flipCard(cardId) {
-	cardsInPlay.push(cards[cardId]);
-	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
 	}
