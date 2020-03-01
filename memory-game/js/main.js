9cd80c91991bf0d70c645d1556abcf82ca391674
Virulious <virulious@hotmail.com>
2020-03-01 06:29:37 -0500

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -22,6 +22,16 @@ let cards = [
 ];
 let cardsInPlay = [];
 
+function createBoard() {
+	for (let i = 0; i < cards.length; i++) {
+		let cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
 function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
   		console.log("You found a match!");
@@ -31,15 +41,16 @@ function checkForMatch() {
 	}
 }
 
-function flipCard(cardId) {
+function flipCard() {
+	const cardId = this.getAttribute('data-id');
 	cardsInPlay.push(cards[cardId].rank);
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
+	this.setAttribute('src', cards[cardId].cardImage);
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
 	}
 }
 
-flipCard(0);
-flipCard(2);
+createBoard();
